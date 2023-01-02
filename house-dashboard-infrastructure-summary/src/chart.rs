1f52b25947b78c95fd1766192f3f08e9dbeab61c
Claudio Giovanni Mattera <dev@claudiomattera.it>
2023-01-02 16:04:19 +0100

    Refactor main function in infrastructure summary chart

--- house-dashboard-infrastructure-summary/src/chart.rs
+++ house-dashboard-infrastructure-summary/src/chart.rs
@@ -6,7 +6,7 @@
 
 //! Functions for generating chart
 
-use tracing::{debug, info};
+use tracing::{debug, info, trace};
 
 use std::collections::{HashMap, HashSet};
 use std::hash::BuildHasher;
@@ -14,8 +14,9 @@ use std::hash::BuildHasher;
 use chrono::{DateTime, Local, Utc};
 
 use plotters::{
-    backend::BitMapBackend,
-    drawing::IntoDrawingArea,
+    backend::{BitMapBackend, DrawingBackend},
+    coord::Shift,
+    drawing::{DrawingArea, IntoDrawingArea},
     element::{Circle, Rectangle, Text},
     style::{
         text_anchor::{HPos, Pos, VPos},
@@ -33,6 +34,21 @@ use crate::Error;
 use crate::InfrastructureSummaryConfiguration;
 use crate::Loadbar;
 
+/// X position of hostname
+const HOST_X: i32 = 50;
+
+/// X position of status
+const STATUS_X: i32 = 220;
+
+/// X position of load
+const LOAD_X: i32 = 280;
+
+/// Maximal load
+const MAX_LOAD: f64 = 1.0;
+
+/// Header height
+const HEADER_HEIGHT: i32 = 35;
+
 /// Draw an infrastructure summary chart
 ///
 /// # Errors
@@ -49,90 +65,124 @@ pub fn draw_infrastructure_summary<S>(
 where
     S: BuildHasher,
 {
-    /// X position of hostname
-    const HOST_X: i32 = 50;
-
-    /// X position of status
-    const STATUS_X: i32 = 220;
-
-    /// X position of load
-    const LOAD_X: i32 = 280;
-
-    /// Maximal load
-    const MAX_LOAD: f64 = 1.0;
-
     info!("Drawing infrastructure summary");
 
-    let title_font = (style.font_name.as_str(), 16.0 * style.font_scale).into_font();
-    let label_font = (style.font_name.as_str(), 8.0 * style.font_scale).into_font();
-
     let root = backend.into_drawing_area();
-    let (width, height) = root.dim_in_pixel();
-
     root.fill(&style.system_palette.pick(SystemColor::Background))?;
 
-    // In order to make room for the colorbar, we need to set `margin_right()`
-    // but that would make the title not centred.
-    // So we must draw the title manually, and also create a new margin area.
+    // Draw the title manually and create a new margin area
+    draw_title(infrastructure_summary.title.as_str(), style, &root)?;
+    let new_root = root.margin(30, 0, 0, 0);
+
+    draw_header(style, &new_root)?;
+
+    let loads: Vec<(&String, Option<f64>)> = prepare_sorted_loads(hosts, loads);
+    draw_hosts(
+        loads.as_slice(),
+        infrastructure_summary.vertical_step.unwrap_or(20),
+        infrastructure_summary.suffix.as_ref(),
+        style,
+        &new_root,
+    )?;
+
+    if let Some(ref last_update_format) = infrastructure_summary.last_update_format {
+        draw_footer(last_update_format, now, style, &new_root)?;
+    }
+
+    Ok(())
+}
+
+/// Prepare sorted pairs (host, load)
+fn prepare_sorted_loads<'a, 'b, S>(
+    hosts: &'a HashSet<String, S>,
+    loads: &'b HashMap<String, f64, S>,
+) -> Vec<(&'a String, Option<f64>)>
+where
+    S: BuildHasher,
+{
+    let mut hosts = hosts.iter().collect::<Vec<&String>>();
+    hosts.sort();
+
+    hosts
+        .into_iter()
+        .map(|host| (host, loads.get(host).copied()))
+        .collect()
+}
+
+/// Draw title
+fn draw_title<DB: DrawingBackend>(
+    title: &str,
+    style: &StyleConfiguration,
+    root: &DrawingArea<DB, Shift>,
+) -> Result<(), Error> {
+    let title_font = (style.font_name.as_str(), 16.0 * style.font_scale).into_font();
     let pos = Pos::new(HPos::Center, VPos::Top);
+
+    let (width, _height) = root.dim_in_pixel();
+
     root.draw(&Text::new(
-        infrastructure_summary.title.as_str(),
+        title,
         (i32::try_from(width)? / 2, 10),
         title_font
             .color(&style.system_palette.pick(SystemColor::Foreground))
             .pos(pos),
     ))?;
 
-    let new_root = root.margin(30, 0, 0, 0);
-    let (_new_width, _new_height) = new_root.dim_in_pixel();
+    Ok(())
+}
 
-    let mut hosts = hosts.iter().collect::<Vec<&String>>();
-    hosts.sort();
-
+/// Draw header
+fn draw_header<DB: DrawingBackend>(
+    style: &StyleConfiguration,
+    root: &DrawingArea<DB, Shift>,
+) -> Result<(), Error> {
     let header_pos = Pos::new(HPos::Center, VPos::Top);
-    let header_font = label_font
+    let header_font = (style.font_name.as_str(), 8.0 * style.font_scale)
+        .into_font()
         .color(&style.system_palette.pick(SystemColor::Foreground))
         .pos(header_pos);
 
-    let host_pos = Pos::new(HPos::Left, VPos::Center);
-    let host_font = label_font
-        .color(&style.system_palette.pick(SystemColor::Foreground))
-        .pos(host_pos);
-
-    let footer_pos = Pos::new(HPos::Right, VPos::Bottom);
-    let footer_font = label_font
-        .color(&style.system_palette.pick(SystemColor::Foreground))
-        .pos(footer_pos);
-
     let text = "HOST";
     let half_width: i32 = (i32::try_from(text.len())? * 8) / 2;
-    new_root.draw(&Text::new(text, (HOST_X, 10), &header_font))?;
-    new_root.draw(&Rectangle::new(
+    root.draw(&Text::new(text, (HOST_X, 10), &header_font))?;
+    root.draw(&Rectangle::new(
         [(HOST_X - half_width, 19), (HOST_X + half_width, 19)],
         style.system_palette.pick(SystemColor::Foreground),
     ))?;
 
     let text = "STATUS";
     let half_width: i32 = (i32::try_from(text.len())? * 8) / 2;
-    new_root.draw(&Text::new(text, (STATUS_X, 10), &header_font))?;
-    new_root.draw(&Rectangle::new(
+    root.draw(&Text::new(text, (STATUS_X, 10), &header_font))?;
+    root.draw(&Rectangle::new(
         [(STATUS_X - half_width, 19), (STATUS_X + half_width, 19)],
         style.system_palette.pick(SystemColor::Foreground),
     ))?;
 
     let text = "LOAD";
     let half_width: i32 = (i32::try_from(text.len())? * 8) / 2;
-    new_root.draw(&Text::new(text, (LOAD_X, 10), &header_font))?;
-    new_root.draw(&Rectangle::new(
+    root.draw(&Text::new(text, (LOAD_X, 10), &header_font))?;
+    root.draw(&Rectangle::new(
         [(LOAD_X - half_width, 19), (LOAD_X + half_width, 19)],
         style.system_palette.pick(SystemColor::Foreground),
     ))?;
 
+    Ok(())
+}
+
+/// Draw hosts
+fn draw_hosts<DB>(
+    loads: &[(&String, Option<f64>)],
+    vertical_step: i32,
+    suffix: Option<&String>,
+    style: &StyleConfiguration,
+    root: &DrawingArea<DB, Shift>,
+) -> Result<(), Error>
+where
+    DB: DrawingBackend,
+{
     let colormap = Colormap::new_with_bounds(Some(ColormapType::Status).as_ref(), 0.0, MAX_LOAD);
 
-    for (i, host) in (0..).zip(hosts) {
-        let load: Option<f64> = loads.get(host).copied();
-
+    for (i, &(host, load)) in (0..).zip(loads) {
         debug!(
             "Processing host {} ({}, relative load: {})",
             i + 1,
@@ -140,54 +190,113 @@ where
             load.map_or_else(|| "None".to_owned(), |l| l.to_string(),)
         );
 
-        let vertical_step = infrastructure_summary.vertical_step.unwrap_or(20);
-        let centered_y = 35 + vertical_step * i;
+        let centered_y = HEADER_HEIGHT + vertical_step * i;
 
-        debug!("Drawing hostname");
-        let short_hostname = match infrastructure_summary.suffix {
-            Some(ref suffix) => host.strip_suffix(suffix).unwrap_or(host),
-            None => host,
-        };
-        new_root.draw(&Text::new(short_hostname, (15, centered_y), &host_font))?;
-
-        debug!("Drawing status");
-        let color = if load.is_some() {
-            colormap.get_color(0.0).to_rgba()
-        } else {
-            colormap.get_color(MAX_LOAD).to_rgba()
-        };
-        let shape_style: ShapeStyle = ShapeStyle {
-            color,
-            filled: true,
-            stroke_width: 0,
-        };
-        new_root.draw(&Circle::new((STATUS_X, centered_y), 7, shape_style))?;
-        new_root.draw(&Circle::new(
-            (STATUS_X, centered_y),
-            7,
-            style.system_palette.pick(SystemColor::LightForeground),
-        ))?;
-
-        debug!("Drawing loadbar");
-        let loadbar = Loadbar::new(
-            (LOAD_X, centered_y),
-            (40, 10),
-            MAX_LOAD,
-            load.unwrap_or(0.0),
-            &style.system_palette,
-            &colormap,
-        );
-        new_root.draw(&loadbar)?;
+        draw_host_name(host, suffix, centered_y, style, root)?;
+        draw_host_status(&colormap, load, centered_y, style, root)?;
+        draw_host_loadbar(&colormap, load, centered_y, style, root)?;
     }
 
-    if let Some(ref format) = infrastructure_summary.last_update_format {
-        let now: DateTime<Local> = now.with_timezone(&Local);
-        new_root.draw(&Text::new(
-            now.format(format).to_string(),
-            (i32::try_from(width)? - 10, i32::try_from(height)? - 30),
-            &footer_font,
-        ))?;
-    }
+    Ok(())
+}
+
+/// Draw host name
+fn draw_host_name<DB: DrawingBackend>(
+    host: &str,
+    suffix: Option<&String>,
+    centered_y: i32,
+    style: &StyleConfiguration,
+    root: &DrawingArea<DB, Shift>,
+) -> Result<(), Error> {
+    trace!("Drawing hostname");
+
+    let host_pos = Pos::new(HPos::Left, VPos::Center);
+    let host_font = (style.font_name.as_str(), 8.0 * style.font_scale)
+        .into_font()
+        .color(&style.system_palette.pick(SystemColor::Foreground))
+        .pos(host_pos);
+
+    let short_hostname = match suffix {
+        Some(suffix) => host.strip_suffix(suffix).unwrap_or(host),
+        None => host,
+    };
+    root.draw(&Text::new(short_hostname, (15, centered_y), &host_font))?;
+
+    Ok(())
+}
+
+/// Draw host status
+fn draw_host_status<DB: DrawingBackend>(
+    colormap: &Colormap,
+    load: Option<f64>,
+    centered_y: i32,
+    style: &StyleConfiguration,
+    root: &DrawingArea<DB, Shift>,
+) -> Result<(), Error> {
+    trace!("Drawing status");
+    let color = if load.is_some() {
+        colormap.get_color(0.0).to_rgba()
+    } else {
+        colormap.get_color(MAX_LOAD).to_rgba()
+    };
+    let shape_style: ShapeStyle = ShapeStyle {
+        color,
+        filled: true,
+        stroke_width: 0,
+    };
+    root.draw(&Circle::new((STATUS_X, centered_y), 7, shape_style))?;
+    root.draw(&Circle::new(
+        (STATUS_X, centered_y),
+        7,
+        style.system_palette.pick(SystemColor::LightForeground),
+    ))?;
+
+    Ok(())
+}
+
+/// Draw host load bar
+fn draw_host_loadbar<DB: DrawingBackend>(
+    colormap: &Colormap,
+    load: Option<f64>,
+    centered_y: i32,
+    style: &StyleConfiguration,
+    root: &DrawingArea<DB, Shift>,
+) -> Result<(), Error> {
+    trace!("Drawing loadbar");
+    let loadbar = Loadbar::new(
+        (LOAD_X, centered_y),
+        (40, 10),
+        MAX_LOAD,
+        load.unwrap_or(0.0),
+        &style.system_palette,
+        colormap,
+    );
+    root.draw(&loadbar)?;
+
+    Ok(())
+}
+
+/// Draw footer
+fn draw_footer<DB: DrawingBackend>(
+    last_update_format: &str,
+    now: DateTime<Utc>,
+    style: &StyleConfiguration,
+    root: &DrawingArea<DB, Shift>,
+) -> Result<(), Error> {
+    let (width, height) = root.dim_in_pixel();
+
+    let footer_pos = Pos::new(HPos::Right, VPos::Bottom);
+    let footer_font = (style.font_name.as_str(), 8.0 * style.font_scale)
+        .into_font()
+        .color(&style.system_palette.pick(SystemColor::Foreground))
+        .pos(footer_pos);
+
+    let now: DateTime<Local> = now.with_timezone(&Local);
+    root.draw(&Text::new(
+        now.format(last_update_format).to_string(),
+        (i32::try_from(width)? - 10, i32::try_from(height)?),
+        &footer_font,
+    ))?;
 
     Ok(())
 }
